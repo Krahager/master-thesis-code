be03a55730fdb30861527f8940dedac1b22af62a
Christopher Kragebøl Hagerup <cha113@post.uit.no>
2019-06-08 01:26:12 +0200

    Cleaned up wellsdemo and added comments

--- multitest/wellsdemo.py
+++ multitest/wellsdemo.py
@@ -9,17 +9,22 @@ imap = map
 
 from sklearn.metrics import classification_report
 
-directory = "/home/krahager/PyUiTestResults/MultiDetectionTest/demo"
+# Path to validation/demo directory
+# The images need to be in a subdirectory of directory given by the path below
+directory = "/path/to/demo/"
 
+# Get import path for model. Model path should point to the model you wish to test
 model_dir = os.path.join(os.getcwd(), 'saved_models')
-model_name = 'wells_full_vgg.hdf5'
+model_name = 'wells.hdf5'
 
 model_path = os.path.join(model_dir, model_name)
 
-shape=(224,224)
+# Input shape should match input shape of the trained model
+# An earlier well model was set to (224, 224) due to a typo
+shape=(214,214)
 resize_shape=(shape[0],shape[1],3)
 
-#from keras.datasets import cifar10
+# Use ImageDataGenerator to load the images
 datagen = ImageDataGenerator(validation_split=None)
 # train_gen = datagen.flow_from_directory(
 #         directory,
@@ -40,17 +45,22 @@ val_gen = datagen.flow_from_directory(
         subset=None
         )
 
+# Find amount of image in the subfolders
 names = val_gen.filenames
 step = len(names)
 
+# Load the model
 cifar_model = keras.models.load_model(model_path)
 
 num_classes = 2
 
 print(len(names))
 
+# Predict classes for images
+# For each image, returns [prob true, prob false]
 predicted_classes = cifar_model.predict_generator(val_gen, steps=step)
 
+# Print the predictions
 print("Printing predictions for horizon model: [true | false]")
 for x in range(len(names)):
     print("File " + names[x] + " result: " + str(predicted_classes[x]))
